db88b0f7b33090b07916b651d473af542b45a959
as <aoyuu.920.96@gmail.com>
2021-02-06 12:13:48 +0900

    プレイヤーを動かすスクリプト(Rigidbodyを使用)

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    float x;
+    float z;
+    public float moveSpeed = 2f;
+
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        x = Input.GetAxisRaw("Horizontal");
+        z = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(x, 0, z) * moveSpeed;
     }
 }
