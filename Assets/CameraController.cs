3ab1713fcfac8fc9f52e28bb931a4aea3a969613
as <aoyuu.920.96@gmail.com>
2021-02-06 14:55:09 +0900

    コメント追加

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private GameObject player;
-    private Vector3 offset;
+    private GameObject player;  //プレイヤー情報格納用
+    private Vector3 offset; // 相対距離取得用
 
     void Start()
     {
+        //　Playerの情報を取得
         this.player = GameObject.Find("Player");
 
+        // メインカメラ（自分自身）とPlayerとの相対距離を求める
         offset = transform.position - player.transform.position;
     }
 
     void Update()
     {
+        //　新しいトランスフォームの値を代入する
         transform.position = player.transform.position + offset;
     }
 }
